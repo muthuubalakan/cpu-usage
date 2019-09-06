4d43619f2c54c69ecf91418b4f6dbeaa8d5680c2
muthuubalakan <muthuumuthu@gmail.com>
2019-09-06 16:34:11 +0200

    add help text

--- src/sread.cpp
+++ src/sread.cpp
@@ -5,6 +5,24 @@
 using namespace std;
 
 
+/*
+sum up all the columns in the 1st line "cpu" :
+( user + nice + system + idle + iowait + irq + softirq )
+this will yield 100% of CPU time
+
+calculate the average percentage of total 'idle' out of 100% of CPU time :
+( user + nice + system + idle + iowait + irq + softirq ) = 100%
+( idle ) = X %
+
+hence
+average idle percentage X % = ( idle * 100 ) / ( user + nice + system + idle + iowait + irq + softirq )
+
+
+Based on the example above:
+average idle percentage X % = ( 842486413 * 100 ) / ( 79242 + 0 + 74306 + 842486413 + 756859 + 6140 + 67701 + 0 ) = ( 842486413100 ) / ( 843470661 ) = 99.8833 %
+
+*/
+
 void load_array(string line){
 	string del = " ";
 	string cpu = "cpu";
